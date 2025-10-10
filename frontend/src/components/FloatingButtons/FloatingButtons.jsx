0d5265d50061162c58061119c3590b5c0a4470e4
Dinhlam0308 <dinhlam030805@gmail.com>
2025-10-11 02:07:25 +0700

    Create FloatingButtons.jsx

---
+++ frontend/src/components/FloatingButtons/FloatingButtons.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { FaPhoneAlt } from "react-icons/fa";
+import { SiZalo } from "react-icons/si";
+import "./FloatingButtons.css";
+
+export default function FloatingButtons() {
+  return (
+    <div className="floating-buttons">
+      {/* Nút Zalo */}
+      <a
+        href="https://zalo.me/0968992882" // 👉 Thay bằng số Zalo của bạn
+        className="btn-zalo"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <SiZalo className="icon" />
+      </a>
+
+      {/* Nút gọi điện */}
+      <a href="tel:0968992882" className="btn-phone">
+        <FaPhoneAlt className="icon" />
+      </a>
+    </div>
+  );
+}
