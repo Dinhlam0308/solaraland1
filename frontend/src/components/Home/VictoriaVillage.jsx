dde7851ce05aeb5729d6a599a3c67323d63737e6
Dinhlam0308 <dinhlam030805@gmail.com>
2025-10-11 02:02:22 +0700

    Update VictoriaVillage.jsx

--- frontend/src/components/Home/VictoriaVillage.jsx
+++ frontend/src/components/Home/VictoriaVillage.jsx
@@ -23,7 +23,7 @@ export default function VictoriaVillagePage() {
     const [currentPage, setCurrentPage] = useState(1);
     const productsPerPage = 8;
     const navigate = useNavigate();
-    const zaloNumber = "0123456789";
+    const zaloNumber = "0968992882";
 
     useEffect(() => {
         getProjectWithSaleProducts(victoriaProjectId).then((data) => {
